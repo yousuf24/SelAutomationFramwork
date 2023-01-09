d6c99b4ef6a9a2d0409f634016b5228ad5fe4ff9
yousuf <aqieb.javed1996@gmail.com>
2023-01-09 18:03:41 +0530

    Day14 javaBasics

---
+++ DonotUse/src/com/au/Day14.java
@@ -0,0 +1,68 @@
+package com.au;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+public class Day14 extends Thread {
+	
+	//abstract void absM();//declaration
+	
+//	static
+//		{ System.out.println("My name is syed"); method23();		}
+	public static void main(String[] args) {
+		
+		float[] arr= {1.0f,2.3f,3.5f};//array is fixed in nature
+		float[] arr1=new float[3];//for
+		
+		
+		
+//		String ref1="arshiya";// it already has object created and it points that object. It doesn't create an object
+//		String ref2="arshiya";
+//		String ref3=new String("arshiya");//it creates an object
+//		String ref4=new String("arshiya");
+//		System.out.println(ref1==ref2);//true . ref1.equals(ref2); true
+//		System.out.println(ref3==ref4);//false  ref3.equals(ref4); true
+		
+		//Immutable
+//		String ref5="abc";
+//		System.out.println(ref5+"d");//print the value
+//		System.out.println(ref5);//we are checking if our ref5 is changed or not
+//		//Mutable
+//		System.out.println("****************************");
+//		StringBuilder ref6=new StringBuilder("abc");
+//		System.out.println(ref6.append("d"));
+//		System.out.println(ref6);
+		
+		
+		
+//		String name="joey";//initialization
+//		String fn;//declaration
+//		name="chandler";//assignment. preRequisite is it should have been declared/initialized
+//		fn="phoebe";
+//		System.out.println(name+":"+fn);
+		
+		//System.out.println(Arrays.toString(args));//Arrays.toString()
+//		for(int i=0;i<args.length;i++)
+//			System.out.println(args[i]);
+		
+//		int[][] matrix=new int[2][3];
+//		//for
+//		int[][] matrix2= {{1,2,3},{4,5,6}};// no of subbraces= rowNumber/rowsize
+//		
+//		int[] ref= {1,2,3,4};
+//		int[] ref2=new int[4];
+//		for(int i=0;i<ref2.length;i++) {
+//			ref2[i]=i+1;
+//		}
+
+		
+	}
+
+
+	static void method23() {
+		System.out.println("inside method23");
+	}
+
+
+	
+}
--- ForJava/src/com/codility/LongestBinarySeq.java
+++ ForJava/src/com/codility/LongestBinarySeq.java
@@ -1,6 +1,28 @@
 package com.codility;
 
 public class LongestBinarySeq {
+	static int maxZeros(int N)
+    {
+        int maxm = -1;
+        
+        String bs=Integer.toBinaryString(N);int len=bs.length();
+        System.out.println(bs);
+        for(int i=0;i<len;i++) {
+        	if(bs.charAt(i)=='1') {
+        		int cnt = 0;//make it zero when u hit '1' closing end
+        		int j=i+1;
+        		while(j<len) {
+        			if(bs.charAt(j)=='1') {cnt=j-(i+1); break;}
+        			else j++;
+        		}maxm=Math.max(maxm, cnt);
+        		i=Math.max(j-1, i);
+        		//System.out.println(i);
+        	}
+        }
+       
+
+        return maxm;
+    }
 	public static int solution(int N) {
 		String bs=Integer.toBinaryString(N);
 		System.out.println(bs);
@@ -15,18 +37,19 @@ public class LongestBinarySeq {
 				while (j<len) {
 					 if (bs.charAt(j) == '1') {
 						lmax = j - (i+1);break;						
-					}else {j++;}
+					}else j++;
 				}
-				System.out.println(lmax);
+				
 				gmax = Math.max(gmax, lmax);
-				i=Math.max(j, i);
+				i=Math.max(j-1, i);
 			}i++;
 			
 		}return gmax;
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(solution(75826520));//10000010001 = 1041
+		
+		System.out.println(solution(104198));//10000010001 = 1041
+		System.out.println(maxZeros(104197));
 
 	}
 
